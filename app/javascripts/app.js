5ac31531afc0278b0f2b089b7cecd3f9391230a3
Vladislav Klimenko <sunsingerus@gmail.com>
2017-12-09 17:17:00 +0300

    migrate to truffle

---
+++ app/javascripts/app.js
@@ -0,0 +1,63 @@
+// Import the page's CSS. Webpack will know what to do with it.
+import "../stylesheets/app.css";
+
+// Import libraries we need.
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
+
+// Import our contract artifacts and turn them into usable abstractions.
+import voting_artifacts from '../../build/contracts/Voting.json'
+
+var Voting = contract(voting_artifacts);
+
+let candidates = {"Mama": "candidate-1", "Mila": "candidate-2", "Rama": "candidate-3"}
+
+window.voteForCandidate = function(candidate) {
+  let candidateName = $("#candidate").val();
+  try {
+    $("#msg").html("Vote " + candidateName + " has been submitted. The vote count will increment as soon as the vote is recorded on the blockchain. Please wait.")
+    $("#candidate").val("Processing vote for " + candidateName);
+
+    /* Voting.deployed() returns an instance of the contract. Every call
+     * in Truffle returns a promise which is why we have used then()
+     * everywhere we have a transaction call
+     */
+    Voting.deployed().then(function(contractInstance) {
+      contractInstance.voteFor(candidateName, {gas: 1000000, from: web3.eth.accounts[0]}).then(function() {
+
+        $("#candidate").val("Voted for" + candidateName);
+
+        let div_id = candidates[candidateName];
+        contractInstance.getVotesFor.call(candidateName).then(function(v) {
+          $("#" + div_id).html(v.toString());
+          $("#msg").html("");
+        });
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+$( document ).ready(function() {
+  if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source like Metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+
+  Voting.setProvider(web3.currentProvider);
+  let candidateNames = Object.keys(candidates);
+  for (var i = 0; i < candidateNames.length; i++) {
+    let name = candidateNames[i];
+    Voting.deployed().then(function(contractInstance) {
+      contractInstance.getVotesFor.call(name).then(function(v) {
+        $("#" + candidates[name]).html(v.toString());
+      });
+    })
+  }
+});
